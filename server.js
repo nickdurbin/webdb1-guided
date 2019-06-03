d3b9a9dd184d4a5309e6bd226874f34a3f9a79e5
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-06-03 18:32:56 +0100

    post router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const PostRouter = require('./posts/post-router.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', PostRouter);
 
 server.get('/', (req, res) => {
   res.send('<h3>DB Helpers with knex</h3>');
