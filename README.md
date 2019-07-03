bd8809459a5a3f12924e023d39b0dedf3e0537c5
Luis Hernandez <luishrd@gmail.com>
2019-07-03 16:32:13 -0600

    renames database and updates instructions

--- README.md
+++ README.md
@@ -12,6 +12,4 @@ In this project we will cover the basics of `Structure Query Language (SQL)`, `R
 
 - [ ] fork and clone this repository.
 - [ ] **CD into the folder** where you cloned **your fork**.
-- [ ] type `yarn` or `npm i` to download dependencies.
-
-Please follow along as the instructor builds the database helpers step by step.
+- [ ] type `npm i` to download dependencies.
--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,15 @@
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/posts.sqlite3'
+      filename: './data/posts.db3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations'
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './data/seeds'
-    }
+      directory: './data/seeds',
+    },
   },
-
 };
