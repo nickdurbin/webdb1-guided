fa877d4d3a5a795b220604e9c7a6a0a291d6e96c
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-07-06 18:17:32 -0700

    db boilerplate

---
+++ posts/post-model.js
@@ -0,0 +1,29 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove
+};
+
+function find() {
+
+}
+
+function findById(id) {
+
+}
+
+function add(post) {
+
+}
+
+function update(id, changes) {
+
+}
+
+function remove(id) {
+
+}
